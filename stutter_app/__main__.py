d71585f9a0757498a6e0bb47674f2792344f3ca2
Kyle <61925456+Zero2164@users.noreply.github.com>
2024-09-09 17:28:57 +1000

    add stutter app

---
+++ stutter_app/__main__.py
@@ -0,0 +1,15 @@
+actual_param, expected_param = [
+	"increasing", "adventures", "enticing", "unacceptable", "accountable", "incredible", "exquisite",
+	"am", "enduring", "outstanding", "astonishing", "astounding", "impressive", "revolutionize",
+	"recurring", "recollection", "so", "gorgeous", "captivating"
+], [
+	"in... in... increasing?", "ad... ad... adventures?", "en... en... enticing?", "un... un... unacceptable?",
+    "ac... ac... accountable?", "in... in... incredible?", "ex... ex... exquisite?", "am... am... am?",
+    "en... en... enduring?", "ou... ou... outstanding?", "as... as... astonishing?", "as... as... astounding?",
+    "im... im... impressive?", "re... re... revolutionize?", "re... re... recurring?", "re... re... recollection?",
+    "so... so... so?", "go... go... gorgeous?", "ca... ca... captivating?",
+]
+
+def stutter(word):
+	print(word)
+ 
